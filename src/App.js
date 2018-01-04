1e3294759b6cfa9eb92d2fd9d1d5bbd08463bab8
Stuart Parton <stuartparton@gmail.com>
2018-01-04 19:10:56 +1100

    update to styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import piggy from './piggy.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +8,37 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <img src={piggy} className="piggy-logo" alt="piggy" />
+
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">Welcome to Dinkum Coin</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, select your wallet below
         </p>
+      <div>
+      <select>
+      <option value="" selected disabled hidden>Choose your wallet here...</option>
+        <option value="stu">Stu</option>
+        <option value="indie">Indie</option>
+      </select>
+
+      </div>
+      <br/> 
+      <hr/>
+      <div>
+      <label>Welcome [blah] </label>
+      </div>
+      <br/>
+
+      <div>
+        <label>Your coin balance is </label>
+        <label>50 </label>
+      </div>
+      <br/>
+      <div>
+      <button>mine a coin?</button>
+      </div>
       </div>
     );
   }
